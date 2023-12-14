8a4a162101f681ba6ffada579d4d1d564ea2db19
blackoutgenie <danieltomiwa95@gmail.com>
2023-12-13 18:22:42 -0800

    Simple shell

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,19 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
-* malloc_checked -  allocates memory using malloc.
-* @b: size
-* Return: pointer
-*/
+ * malloc_checked -  allocates memory using malloc.
+ *@b: size
+ *Return: pointer
+ */
 
 void *malloc_checked(unsigned int b)
-{
-	char *output;
 
-	output = malloc(b);
-	if (output == NULL)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
 		exit(98);
-	else
-		return (output);
+	return (p);
 }
+
--- simple_shell/execute.c
+++ simple_shell/execute.c
@@ -7,38 +7,32 @@
  * Return: 0
  */
 
-void execute_command(const char *command)
-{
-	pid_t child_pid = fork();
+void execute_command(const char *command) {
+    pid_t child_pid = fork();
 
-	if (child_pid == -1)
-	{
-		tom_print("Error forking process.\n");
-		exit(EXIT_FAILURE);
-	}
-	else if (child_pid == 0)
-	{
-		char *args[120];
-		int arg_count = 0;
+    if (child_pid == -1) {
+        tom_print("Error forking process.\n");
+        exit(EXIT_FAILURE);
+    } else if (child_pid == 0) {
+        char *args[120];
+        int arg_count = 0;
 
-		char *token = strtok((char *)command, " ");
+        char *token = strtok((char *)command, " ");
 
+        while (token != NULL) {
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
 
-		while (token != NULL)
-		{
-			args[arg_count++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[arg_count] = NULL;
+        args[arg_count] = NULL;
 
-		execvp(args[0], args);
-
-		tom_print("Error executing command.\n");
-		exit(EXIT_FAILURE);
-	}
-	else if (wait(NULL) == -1)
-	{
-		perror("Error waiting for child process.\n");
-		exit(EXIT_FAILURE);
-	}
+        if (execve(args[0], args, NULL) == -1) {
+            tom_print("Error executing command.\n");
+            exit(EXIT_FAILURE);
+        }
+    } else if (wait(NULL) == -1) {
+        perror("Error waiting for child process.\n");
+        exit(EXIT_FAILURE);
+    }
 }
+
